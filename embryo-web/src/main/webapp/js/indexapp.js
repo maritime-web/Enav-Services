b8c8c6c07e52932476d4f1da0dea29f1ebb7dcb3
Andreas Borchsenius Westh <andreas@lundogbendsen.dk>
2016-01-29 13:57:00 +0100

    removing web module

--- embryo-web/src/main/webapp/js/indexapp.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    "use strict";
-    var indexApp = angular.module('embryo.front', [ 'embryo.menu', 'ui.bootstrap.carousel' ]);
-    $(function() {
-        embryo.authentication.currentPageRequiresAuthentication = false;
-    });
-})();
-
