3deec4015799d730cbf52b83143d372816a24ef3
steen-elv <steen@lundogbendsen.dk>
2017-07-31 15:45:30 +0200

    Fixed accidental commit of unittests

--- service-registry-static/src/test/java/dk/dma/enav/services/registry/StaticServiceRegistryTest.java
+++ service-registry-static/src/test/java/dk/dma/enav/services/registry/StaticServiceRegistryTest.java
@@ -57,38 +57,4 @@ public class StaticServiceRegistryTest {
         cut.getServiceInstances(new TechnicalDesignId("", ""), "");
     }
 
-    @Test
-    public void name() throws Exception {
-        String xpathExp = "//div[@class='scrollable']//tbody/tr/td[2]/text()";
-        BufferedReader reader = com.google.common.io.Files.newReader(new File("C:\\dev\\projekter\\Enav-Services\\prod_emails.xml"), Charset.forName("UTF-8"));
-        XPath xPath = XPathFactory.newInstance().newXPath();
-        NodeList res = (NodeList) xPath.evaluate(xpathExp, new InputSource(reader), XPathConstants.NODESET);
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < res.getLength(); i++) {
-            sb.append(res.item(i).getNodeValue()).append(",");
-        }
-        System.out.println(sb);
-    }
-
-    @Test
-    public void aaa() throws Exception {
-/*
-        double a = 3601/499364868000D;
-        double b = 30390001/249682434000D;
-        double c = 1858368270063083D/1857916052584778D;
-*/
-        double a = 3601/998729736000D;
-        double b = 30390001/499364868000D;
-        double c = 6690306659218421D/14863328420678224D;
-
-        System.out.println("a: " + a);
-        System.out.println("b: " + b);
-        System.out.println("c: " + c);
-
-        double[] resolutions = {2, 100, 500, 1000, 2000, 3000, 4000, 9000, 9800};
-
-        for (double resolution : resolutions) {
-            System.out.println("resolution: " + resolution + " => " + (a*resolution*resolution - b*resolution + c));
-        }
-    }
 }
