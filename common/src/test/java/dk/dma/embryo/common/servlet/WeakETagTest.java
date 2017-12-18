a0100a876283c2583c8e66641ef80fd2dcb2a138
steen-elv <steen@lundogbendsen.dk>
2017-12-18 14:49:45 +0100

    MARWEB-566 fixed checkstyle bug

--- common/src/test/java/dk/dma/embryo/common/servlet/WeakETagTest.java
+++ common/src/test/java/dk/dma/embryo/common/servlet/WeakETagTest.java
@@ -18,7 +18,7 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class WeakETagTest {
     @Test
