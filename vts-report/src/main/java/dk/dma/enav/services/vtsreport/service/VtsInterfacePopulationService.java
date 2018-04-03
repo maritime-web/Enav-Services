622d8ab71a95ea72f427772994aed26af6834f12
RolandBeeres <roland.beeres@gmail.com>
2018-04-03 17:48:56 +0200

    fix a checkstyle issue in copyright notice by removing it

--- vts-report/src/main/java/dk/dma/enav/services/vtsreport/service/VtsInterfacePopulationService.java
+++ vts-report/src/main/java/dk/dma/enav/services/vtsreport/service/VtsInterfacePopulationService.java
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package dk.dma.enav.services.vtsreport.service;
 
 import javax.ws.rs.GET;
--- vts-report/src/main/java/dk/dma/enav/services/vtsreport/service/VtsReportForwardingService.java
+++ vts-report/src/main/java/dk/dma/enav/services/vtsreport/service/VtsReportForwardingService.java
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package dk.dma.enav.services.vtsreport.service;
 
 import com.google.common.base.Strings;
